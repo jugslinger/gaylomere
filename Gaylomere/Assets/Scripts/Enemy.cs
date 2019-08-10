526aebeb9c43fc85809f334e30e4b9bbf5fcb0bc
jugslinger <smilz4money@aol.com>
2019-08-10 13:49:55 -0700

    0.4.0
    +Multiplayer finished
    +Started on Enemy AI to test combat

---
+++ Gaylomere/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    float pauseTimer;
+    Vector3 moveDistance;
+
+    void Update()
+    {
+        do
+        {
+            moveDistance.x = Random.Range(-6, 7);
+        } while (moveDistance.x == 0);
+        transform.position = Vector2.MoveTowards(transform.position, transform.position + moveDistance, Time.deltaTime);
+
+        pauseTimer = Random.Range(1, 6);
+        do
+        {
+            pauseTimer -= Time.deltaTime;
+        } while (pauseTimer > 0);
+
+    }
+}
